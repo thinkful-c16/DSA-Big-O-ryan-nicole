ff5922f333e9005c0a91f5bfcfd0bf4980ff40d5
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-23 08:33:59 -0800

    Solved the doubler question

--- big-o-drills.js
+++ big-o-drills.js
@@ -16,9 +16,18 @@ function areYouHere(arr1, arr2) {
   for (let i=0; i<arr1.length; i++) {
     const el1 = arr1[i];
     for (let j=0; j<arr2.length; j++) {
-        const el2 = arr2[j];
-        if (el1 === el2) return true;
+      const el2 = arr2[j];
+      if (el1 === el2) return true;
+    }
   }
+  return false;
 }
-return false;
+
+//doubler - O(n) Linear
+
+function doubleArrayValues(array) {
+  for (let i=0; i<array.length; i++) {
+    array[i] *= 2;
+  }
+  return array;
 }
