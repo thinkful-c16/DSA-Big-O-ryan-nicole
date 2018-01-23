0622591633231be30c1a23231aa13b91097de1f7
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-23 08:59:59 -0800

    Efficient Search answer added as logarithmic

--- big-o-drills.js
+++ big-o-drills.js
@@ -77,3 +77,28 @@ function generateFib(num) {
   // we return `result`.
   return result;
 }
+
+//An Efficient Search - Logarithmic time O(log(n))
+
+function efficientSearch(array, item) {
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
+  let currentIndex;
+  let currentElement;
+
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = array[currentIndex];
+
+    if (currentElement < item) {
+      minIndex = currentIndex + 1;
+    }
+    else if (currentElement > item) {
+      maxIndex = currentIndex - 1;
+    }
+    else {
+      return currentIndex;
+    }
+  }
+  return -1;
+}
