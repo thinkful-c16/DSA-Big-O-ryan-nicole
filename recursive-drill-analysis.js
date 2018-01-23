441f30728e3dd878f9194a768609227991a85d68
Ryan Sabo <sabora920@gmail.com>
2018-01-23 14:44:36 -0500

    added explanations

--- recursive-drill-analysis.js
+++ recursive-drill-analysis.js
@@ -1,7 +1,7 @@
-//Counting Sheep - Linear
+//Counting Sheep - Linear - the number of sheep entered directly correlate with the number of times the function is run.
 
-//Doubling Array - Linear
+//Doubling Array - Linear - the amount of indexes in the array directly correlates to the number of times the function is run to double each index
 
-//Reverse String - Linear 
+//Reverse String - Linear - the number of characters within the string directly correlates to how many times the function has to recur in order to successfully reverse the string
 
 //
