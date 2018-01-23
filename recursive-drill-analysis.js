65bcce2d3c7f0c839afb3e609b52c3fbfc509bfe
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-23 12:04:02 -0800

    added explanation and answer for the nth triangular number

--- recursive-drill-analysis.js
+++ recursive-drill-analysis.js
@@ -4,4 +4,4 @@
 
 //Reverse String - Linear - the number of characters within the string directly correlates to how many times the function has to recur in order to successfully reverse the string
 
-//
+//Nth Triangular Number - Linear - As you increment the number of your input, the run time increases proportionally
